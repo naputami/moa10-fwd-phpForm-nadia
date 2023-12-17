28e67e689e461e84e4955f903d11b1bc7901e8cb
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-12-17 18:38:40 +0700

    fix margin and add navbar

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once 'connection.php';
 
-$query = "SELECT sessions.sessionID, sessions.session_date, students.name AS student_name, teachers.name AS teacher_name, teachers.subject from sessions inner join students on sessions.studentID = students.studentID inner join teachers on sessions.teacherID = teachers.teacherID";
+$query = "SELECT sessions.sessionID, students.level,  sessions.session_date, students.name AS student_name, teachers.name AS teacher_name, teachers.subject from sessions inner join students on sessions.studentID = students.studentID inner join teachers on sessions.teacherID = teachers.teacherID";
 $result = $conn->query($query);
 
 ?>
@@ -16,18 +16,44 @@ $result = $conn->query($query);
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="./styles.css" rel="stylesheet">
     <title>Private Courses Data</title>
   </head>
   <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MyCourses</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Sessions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./students/index.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./teachers/index.php">Teachers</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-    <div class="container">
+    <div class="container w-75 mt-3">
       <h1>Private Courses Session Data</h1>
-      <a href="create.php" class="btn btn-primary">Add New Session</a>
-      <table class="table table-striped">
+      <a href="create.php" class="btn btn-success position-absolute end-0 round-button">
+        <svg xmlns="http://www.w3.org/2000/svg" class="mt-3" width="30" height="30" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+        </svg>
+      </a>
+      <table class="table table-light table-hover mt-3">
         <tr>
           <th>No</th>
           <th>Student name</th>
+          <th>Level</th>
           <th>Teacher name</th>
           <th>Session Date</th>
           <th>Subject</th>
@@ -43,12 +69,22 @@ $result = $conn->query($query);
                 <tr>
                   <td><?php echo $index ?></td>
                   <td><?php echo $row['student_name'] ?></td>
+                  <td><?php echo $row['level'] ?></td>
                   <td><?php echo $row['teacher_name'] ?></td>
                   <td><?php echo $row['session_date'] ?></td>
                   <td><?php echo $row['subject'] ?></td>
                   <td>
-                    <a href="<?php echo 'edit.php?id=' . $row['sessionID'] ?>" class="btn btn-secondary">Edit</a>
-                    <a href="<?php echo 'delete.php?id=' . $row['sessionID'] ?>" class="btn btn-danger">Delete</a>
+                    <a href="<?php echo 'edit.php?id=' . $row['sessionID'] ?>" class="btn btn-warning" title="Edit data">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                      </svg>
+                    </a>
+                    <a href="<?php echo 'delete.php?id=' . $row['sessionID'] ?>" class="btn btn-danger" title="Hapus data">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                      </svg>
+                    </a>
                 </td>
                 </tr>
              <?php 
