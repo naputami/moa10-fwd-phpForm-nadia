fd29b21a55dc35c4dbe584623215f6e4faf693b0
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-12-17 18:39:28 +0700

    add page for displaying teacher's data

---
+++ teachers/index.php
@@ -0,0 +1,112 @@
+<?php 
+
+require_once '../connection.php';
+
+$query = "SELECT * FROM teachers";
+$result = $conn->query($query);
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="./styles.css" rel="stylesheet">
+    <title>Private Courses Data</title>
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">MyCourses</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="../index.php">Sessions</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../students/index.php">Students</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Teachers</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+    <div class="container w-75 mt-3">
+      <h1>Private Courses Teacher Data</h1>
+      <a href="create.php" class="btn btn-success position-absolute end-0 round-button">
+        <svg xmlns="http://www.w3.org/2000/svg" class="mt-3" width="30" height="30" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+        </svg>
+      </a>
+      <table class="table table-light table-hover mt-3">
+        <tr>
+          <th>No</th>
+          <th>Teacher name</th>
+          <th>Address</th>
+          <th>Phone</th>
+          <th>Subject</th>
+          <th>Action</th>
+        </tr>
+
+        <?php 
+            if ($result->num_rows > 0) {
+              // output data of each row
+              $index = 1;
+              while($row = $result->fetch_assoc()) {
+                ?> 
+                <tr>
+                  <td><?php echo $index ?></td>
+                  <td><?php echo $row['name'] ?></td>
+                  <td><?php echo $row['address'] ?></td>
+                  <td><?php echo $row['phone'] ?></td>
+                  <td><?php echo $row['subject'] ?></td>
+                  <td>
+                    <a href="<?php echo 'edit.php?id=' . $row['teacherID'] ?>" class="btn btn-warning" title="Edit data">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                        <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                      </svg>
+                    </a>
+                    <a href="<?php echo 'delete.php?id=' . $row['teacherID'] ?>" class="btn btn-danger" title="Hapus data">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
+                      </svg>
+                    </a>
+                </td>
+                </tr>
+             <?php 
+              $index++;
+             }
+            } else {
+              ?> 
+              <tr>
+                  <td colspan="4">No data here!</td>
+              </tr>
+            <?php } ?>
+        
+      </table>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
