331efd2fdddbf220a5e9daba9ede6ed5dc64263a
Nadia Puji Utami <nadiapujiutami@gmail.com>
2023-12-17 22:00:23 +0700

    edit navbar

--- teachers/index.php
+++ teachers/index.php
@@ -29,7 +29,7 @@ $result = $conn->query($query);
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="../index.php">Sessions</a>
+          <a class="nav-link" aria-current="page" href="../sessions/index.php">Sessions</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="../students/index.php">Students</a>
